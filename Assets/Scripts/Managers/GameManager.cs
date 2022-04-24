0af326e8cf6889128db7a76bc4ca022129953aa1
Pedro Longo <pedroolongo.8@gmail.com>
2022-04-24 02:51:01 -0700

    first upload

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,319 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public int m_NumRoundsToWin = 5;        
+    public float m_StartDelay = 3f;         
+    public float m_EndDelay = 3f;           
+    public CameraControl m_CameraControl;   
+    public Text m_MessageText;              
+    public GameObject m_TankPrefab;         
+    public TankManager[] m_Tanks;
+
+    public Transform m_SpawnPoint1;
+    public Transform m_SpawnPoint2;
+
+
+    private int m_RoundNumber;              
+    private WaitForSeconds m_StartWait;     
+    private WaitForSeconds m_EndWait;       
+    private TankManager m_RoundWinner;
+    private TankManager m_GameWinner;
+
+    private PhotonView view;
+
+
+    const float k_MaxDepenetrationVelocity = float.PositiveInfinity;
+
+    private void Awake()
+    {
+        if (isConnected())
+        {
+            SpawnAllTanks();
+            SetCameraTargets();
+
+            StartCoroutine(GameLoop());
+        }
+    }
+    private void Start()
+    {
+        // This line fixes a change to the physics engine.
+        Physics.defaultMaxDepenetrationVelocity = k_MaxDepenetrationVelocity;
+        
+        m_StartWait = new WaitForSeconds(m_StartDelay);
+        m_EndWait = new WaitForSeconds(m_EndDelay);
+
+      
+
+    }
+
+    private bool isConnected()
+    {
+        if (PhotonNetwork.IsConnected)
+            return true;
+        else
+            return false;
+    }
+
+
+    private void SpawnAllTanks()
+    {
+        //for (int i = 0; i < 2; i++)
+        //{
+        //    m_Tanks[i].m_Instance = PhotonNetwork.Instantiate(m_TankPrefab.name, m_SpawnPoint1.transform.position, m_SpawnPoint1.transform.rotation);
+        //    if(m_Tanks.Length == 2){
+        //        m_Tanks[i].m_Instance = PhotonNetwork.Instantiate(m_TankPrefab.name, m_SpawnPoint2.transform.position, m_SpawnPoint2.transform.rotation) as GameObject;
+        //    }
+        //    m_Tanks[i].m_PlayerNumber = i + 1;
+        //    m_Tanks[i].Setup();
+        //}
+    }
+
+
+    private void SetCameraTargets()
+    {
+        Transform[] targets = new Transform[m_Tanks.Length];
+
+        for (int i = 0; i < targets.Length; i++)
+        {
+            targets[i] = m_Tanks[i].m_Instance.transform;
+        }
+
+        m_CameraControl.m_Targets = targets;
+    }
+
+
+    private IEnumerator GameLoop()
+    {
+        yield return StartCoroutine(RoundStarting());
+        yield return StartCoroutine(RoundPlaying());
+        yield return StartCoroutine(RoundEnding());
+
+        if (m_GameWinner != null)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            StartCoroutine(GameLoop());
+        }
+    }
+
+
+    private IEnumerator RoundStarting()
+    {
+        ResetAllTanks();
+        DisableTankControl();
+
+        m_CameraControl.SetStartPositionAndSize();
+
+        m_RoundNumber++;
+        m_MessageText.text = "ROUND " + m_RoundNumber;
+
+        yield return m_StartWait;
+    }
+
+
+    private IEnumerator RoundPlaying()
+    {
+        EnableTankControl();
+
+        m_MessageText.text = string.Empty;
+
+        while (!OneTankLeft())
+        {
+            yield return null;
+        }
+    }
+
+
+    private IEnumerator RoundEnding()
+    {
+        DisableTankControl();
+
+        m_RoundWinner = null;
+
+        m_RoundWinner = GetRoundWinner();
+
+        if(m_RoundWinner != null)
+        {
+            m_RoundWinner.m_Wins++;
+        }
+
+        m_GameWinner = GetGameWinner();
+
+        string message = EndMessage();
+        m_MessageText.text = message;
+
+        yield return m_EndWait;
+    }
+
+
+    private bool OneTankLeft()
+    {
+        int numTanksLeft = 0;
+
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            if (m_Tanks[i].m_Instance.activeSelf)
+                numTanksLeft++;
+        }
+
+        return numTanksLeft <= 1;
+    }
+
+
+    private TankManager GetRoundWinner()
+    {
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            if (m_Tanks[i].m_Instance.activeSelf)
+                return m_Tanks[i];
+        }
+
+        return null;
+    }
+
+
+    private TankManager GetGameWinner()
+    {
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            if (m_Tanks[i].m_Wins == m_NumRoundsToWin)
+                return m_Tanks[i];
+        }
+
+        return null;
+    }
+
+
+    private string EndMessage()
+    {
+        string message = "DRAW!";
+
+        if (m_RoundWinner != null)
+            message = m_RoundWinner.m_ColoredPlayerText + " WINS THE ROUND!";
+
+        message += "\n\n\n\n";
+
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            message += m_Tanks[i].m_ColoredPlayerText + ": " + m_Tanks[i].m_Wins + " WINS\n";
+        }
+
+        if (m_GameWinner != null)
+            message = m_GameWinner.m_ColoredPlayerText + " WINS THE GAME!";
+
+        return message;
+    }
+
+
+    private void ResetAllTanks()
+    {
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            m_Tanks[i].Reset();
+        }
+    }
+
+
+    private void EnableTankControl()
+    {
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            m_Tanks[i].EnableControl();
+        }
+    }
+
+
+    private void DisableTankControl()
+    {
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            m_Tanks[i].DisableControl();
+        }
+    }
+
+    #region Photon Callbacks
+
+
+    /// <summary>
+    /// Called when the local player left the room. We need to load the launcher scene.
+    /// </summary>
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    #endregion
+
+
+    #region Public Methods
+
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+
+    #endregion
+
+    #region Private Methods
+
+
+    void LoadArena()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+        }
+        Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+        PhotonNetwork.LoadLevel("Main");
+    }
+
+    #endregion
+
+    #region Photon Callbacks
+
+
+    public override void OnPlayerEnteredRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+
+            LoadArena();
+        }
+    }
+
+
+    public override void OnPlayerLeftRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+
+            LoadArena();
+        }
+    }
+
+
+    #endregion
+}
---
+++ Assets/Scripts/Managers/TankManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+
+[Serializable]
+public class TankManager : MonoBehaviourPun
+{
+    public Color m_PlayerColor;            
+    public Transform m_SpawnPoint;         
+    [HideInInspector] public int m_PlayerNumber;             
+    [HideInInspector] public string m_ColoredPlayerText;
+    [HideInInspector] public GameObject m_Instance;          
+    [HideInInspector] public int m_Wins;                     
+
+
+    private TankMovement m_Movement;       
+    private TankShooting m_Shooting;
+    private GameObject m_CanvasGameObject;
+
+    public static GameObject LocalPlayerInstance;
+
+    private PhotonView PV;
+
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+
+        if (PV.IsMine)
+        {
+            TankManager.LocalPlayerInstance = this.gameObject;
+        }
+        // #Critical
+        // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void Setup()
+    {
+        m_Movement = m_Instance.GetComponent<TankMovement>();
+        m_Shooting = m_Instance.GetComponent<TankShooting>();
+        m_CanvasGameObject = m_Instance.GetComponentInChildren<Canvas>().gameObject;
+
+    }
+
+
+    public void DisableControl()
+    {
+        m_Movement.enabled = false;
+        m_Shooting.enabled = false;
+
+        m_CanvasGameObject.SetActive(false);
+    }
+
+
+    public void EnableControl()
+    {
+
+        m_Movement.enabled = true;
+        m_Shooting.enabled = true;
+
+        m_CanvasGameObject.SetActive(true);
+    }
+
+
+    public void Reset()
+    {
+        m_Instance.transform.position = m_SpawnPoint.position;
+        m_Instance.transform.rotation = m_SpawnPoint.rotation;
+
+        m_Instance.SetActive(false);
+        m_Instance.SetActive(true);
+    }
+}
